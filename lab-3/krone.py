a6b6862a680fd99e5c51663537a28acc80647dc5
Kostia <babich.kostia@gmail.com>
2023-05-16 17:13:14 -0700

    lab-3 initial commit

---
+++ lab-3/krone.py
@@ -0,0 +1,26 @@
+"""
+LAB 2
+Kostiantyn Babich, Hyunjong Shin
+This assignment is to make a base class and derived classes and use them to perform several operations
+"""
+
+import currency
+
+
+class Krone(currency.Currency):
+    _name = "Krone"
+
+    def __init__(self, value=0.00):
+        super().__init__(value)
+
+
+    def print(self):
+        """Prints the object in form of xx.yy followed by derived currency name
+
+        ex) 1.23 Krone  2.46 Soum
+        pre: 
+        post: object's value and class name
+        return:
+        """
+        print(f"{self.get_value():.2f} {Krone._name}", end=' ')
+
---
+++ lab-3/link_node.py
@@ -0,0 +1,2 @@
+class LinkNode:
+    pass
